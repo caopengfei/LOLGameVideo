39451f0d6558585d2b1312487a26a1d90b4c906f
rakor <cpf20032008@163.com>
2015-02-11 18:16:27 +0800

    初始化

---
+++ src/cn/rakor/gamevideo/user/PersonEdit.java
@@ -0,0 +1,16 @@
+package cn.rakor.gamevideo.user;
+
+import cn.rakor.gamevideo.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PersonEdit extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_edit);
+        
+    }
+    
+}
